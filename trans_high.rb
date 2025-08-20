cede570bc4f31d892270cd6c68ce9910153fb6c0
shinta <shinta@gx14ac.com>
2025-08-20 12:39:15 +0900

    fix trans_high & scared

--- trans_high.rb
+++ trans_high.rb
@@ -1,7 +1,35 @@
-## これはサイケになれる音楽？
 use_bpm 105
 define :sec do |s|; s * (60.0 / current_bpm); end
 define :fade_to do |fx, amp, fade|; (fade * 4).times { |i| control fx, amp: amp * (i + 1) / (fade * 4).to_f; sleep sec(fade) / (fade * 4) }; end
+
+# ===== 歩く音（一度だけ） =====
+in_thread do
+  with_fx :reverb, room: 0.3, mix: 0.2 do
+    with_fx :lpf, cutoff: 100 do
+      sample "/Users/shinta/git/github.com/gx14ac/mpc/assets/walk-zakuzaku.mp3",
+        amp: 0.7,
+        rate: 1.0,
+        pan: 0
+    end
+  end
+end
+
+# ===== 人間とおります（8秒後に一度だけ） =====
+in_thread do
+  sleep sec(8)
+  with_fx :reverb, room: 0.8, mix: 0.7, damp: 0.4 do
+    with_fx :echo, phase: 1.5, decay: 6, mix: 0.5 do
+      with_fx :lpf, cutoff: 120 do
+        sample "/Users/shinta/git/github.com/gx14ac/mpc/assets/taiga-ningen.mp3",
+          amp: 0.8,
+          rate: 1.0,
+          attack: 0.3,
+          release: 3,
+          pan: 0
+      end
+    end
+  end
+end
 
 # Timeline: [time, amp, fade]
 [[0, 0.8, 0], [8, 0.4, 4], [16, 0.4, 3], [32, 0.7, 8], [48, 0.5, 6], [64, 0.5, 4], [80, 0.6, 5], [112, 0.3, 4]].each_with_index do |(time, amp, fade), i|
@@ -18,18 +46,18 @@ with_fx :level, amp: 0 do |fx_hats|; in_thread do; sleep sec(16); fade_to fx_hat
 live_loop :hats do; 8.times { |i| amp = (i % 2 == 1) ? rrand(0.3, 0.4) : rrand(0.25, 0.35); (one_in(20) ? (sample :drum_cymbal_open, amp: amp * 0.6, rate: rrand(0.99, 1.01), finish: rrand(0.4, 0.6), pan: rrand(-0.4, 0.4)) : (sample :drum_cymbal_closed, amp: amp, rate: rrand(0.99, 1.01), finish: rrand(0.3, 0.5), cutoff: rrand(110, 130), pan: rrand(-0.2, 0.2))); sleep 0.5 }; end; end
 
 # Bass
-with_fx :level, amp: 0 do |fx_bass|; in_thread do; sleep sec(32); fade_to fx_bass, 0.7, 8; end
-live_loop :bass do; use_synth :fm; [:c2, :c2, :eb2, :f2, :g2, :f2, :eb2, :c2].zip([1,0,1,0,1,1,0,1,1,0,0,1,1,0,1,0] * 2).each { |note, hit| play note, amp: rrand(0.1, 0.3), divisor: rrand(1.8, 2.2), depth: rrand(0.8, 1.2), attack: 0.01, release: rrand(0.3, 0.6), cutoff: rrand(70, 90), res: rrand(0.3, 0.6) if hit == 1; sleep 0.25 }; end; end
+with_fx :level, amp: 0 do |fx_bass|; in_thread do; sleep sec(32); fade_to fx_bass, 1.2, 8; end
+live_loop :bass do; use_synth :fm; [:c2, :c2, :eb2, :f2, :g2, :f2, :eb2, :c2].zip([1,0,1,0,1,1,0,1,1,0,0,1,1,0,1,0] * 2).each { |note, hit| play note, amp: rrand(0.5, 0.7), divisor: rrand(1.8, 2.2), depth: rrand(0.8, 1.2), attack: 0.01, release: rrand(0.4, 0.7), cutoff: rrand(85, 105), res: rrand(0.1, 0.3) if hit == 1; sleep 0.25 }; end; end
 
-with_fx :level, amp: 0 do |fx_sub|; in_thread do; sleep sec(48); fade_to fx_sub, 0.5, 6; end
-live_loop :sub_bass do; use_synth :subpulse; [1,0,0,0,1,0,1,0,1,0,0,0,1,0,0,1].each { |hit| play [:c1, :eb1, :f1, :g1].choose, amp: rrand(0.4, 0.6), pulse_width: rrand(0.1, 0.3), cutoff: rrand(40, 60), res: rrand(0.2, 0.4), attack: 0.05, release: rrand(0.8, 1.2) if hit == 1; sleep 0.5 }; end; end
+with_fx :level, amp: 0 do |fx_sub|; in_thread do; sleep sec(48); fade_to fx_sub, 0.8, 6; end
+live_loop :sub_bass do; use_synth :subpulse; [1,0,0,0,1,0,1,0,1,0,0,0,1,0,0,1].each { |hit| play [:c1, :eb1, :f1, :g1].choose, amp: rrand(0.6, 0.7), pulse_width: rrand(0.1, 0.3), cutoff: rrand(50, 70), res: rrand(0.1, 0.2), attack: 0.05, release: rrand(0.8, 1.2) if hit == 1; sleep 0.5 }; end; end
 
 # Effects
 with_fx :level, amp: 0 do |fx_vocal|; in_thread do; sleep sec(64); fade_to fx_vocal, 0.5, 4; end
 live_loop :walk_zakuzaku do; sleep rrand(15, 30); sample "/Users/shinta/git/github.com/gx14ac/mpc/assets/walk-zakuzaku.mp3", amp: rrand(0.4, 0.6), rate: rrand(0.9, 1.1), pan: rrand(-0.6, 0.6), cutoff: rrand(80, 110) if one_in(3); end; end
 
 with_fx :level, amp: 0 do |fx_taiga|; in_thread do; sleep sec(0); fade_to fx_taiga, 0.6, 2; end
-  live_loop :taiga_ningen do; sleep rrand(4, 10); if one_in(2); case rrand_i(1, 5)
+  live_loop :taiga_ningen do; sleep rrand(8, 20); if one_in(4); case rrand_i(1, 5)
       when 1; with_fx :reverb, room: 0.6, mix: 0.4, damp: 0.5 do; with_fx :hpf, cutoff: 80, res: 0.2 do; sample "/Users/shinta/git/github.com/gx14ac/mpc/assets/taiga-ningen.mp3", amp: rrand(0.6, 0.9), rate: rrand(0.95, 1.05), start: rrand(0, 0.1), finish: rrand(0.9, 1), attack: rrand(0.1, 0.3), release: rrand(1, 2), pan: rrand(-0.3, 0.3), cutoff: rrand(100, 125); end; end
       when 2; with_fx :reverb, room: 0.8, mix: 0.6, damp: 0.3 do; with_fx :echo, phase: 0.75, decay: 4, mix: 0.5 do; with_fx :lpf, cutoff: rrand(90, 120), res: 0.3 do; sample "/Users/shinta/git/github.com/gx14ac/mpc/assets/taiga-ningen.mp3", amp: rrand(0.5, 0.7), rate: rrand(0.9, 1.1), start: rrand(0, 0.2), finish: rrand(0.8, 1), attack: rrand(0.2, 0.5), release: rrand(2, 4), pan: rrand(-0.5, 0.5), cutoff: rrand(80, 120); end; end; end
       when 3; with_fx :pitch_shift, pitch: rrand(-3, 3) do; with_fx :flanger, phase: rrand(2, 6), decay: 4, mix: 0.4 do; with_fx :reverb, room: 0.5, mix: 0.3 do; sample "/Users/shinta/git/github.com/gx14ac/mpc/assets/taiga-ningen.mp3", amp: rrand(0.4, 0.6), rate: rrand(0.85, 1.15), start: rrand(0, 0.15), finish: rrand(0.85, 1), attack: rrand(0.3, 0.7), release: rrand(1.5, 3), pan: rrand(-0.4, 0.4), cutoff: rrand(90, 125); end; end; end
